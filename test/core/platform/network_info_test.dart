03a91a3b3003a5fc7c15db5353c1735cfbf6b3a7
laijihua <1164258202@qq.com>
2024-12-25 15:54:13 +0800

    从数据库中获取

---
+++ test/core/platform/network_info_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:my_flutter_app/core/platform/network_info.dart';
+import 'network_info_test.mocks.dart';
+
+@GenerateMocks([
+  InternetConnectionChecker,
+])
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
+
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockInternetConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test(
+      'should forward the call to DataConnectionChecker.hasConnection',
+      () async {
+        // arrange
+        final tHasConnectionFuture = Future.value(true);
+
+        when(mockInternetConnectionChecker.hasConnection)
+            .thenAnswer((_) => tHasConnectionFuture);
+        // act
+        // NOTICE: We're NOT awaiting the result
+        final result = networkInfo.isConnected;
+        // assert
+        verify(mockInternetConnectionChecker.hasConnection);
+        // Utilizing Dart's default referential equality.
+        // Only references to the same object are equal.
+        expect(result, tHasConnectionFuture);
+      },
+    );
+  });
+}
